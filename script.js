becfc969b41ee5e2eaee54a14cb296a822335a59
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-08-09 12:32:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -458,7 +458,7 @@ function submitGuess() {
                 const currentRow = guessRows[currentGuess - 1];
                 const feedbackButton = currentRow.querySelector('.feedback-button');
                 feedbackButton.classList.add('show-tooltip');
-                setTimeout(() => feedbackButton.classList.remove('show-tooltip'), 2200);
+                setTimeout(() => feedbackButton.classList.remove('show-tooltip'), 8000);
                 localStorage.setItem('fermiTooltipTutorialShown', '1');
             }
         } catch (e) {
