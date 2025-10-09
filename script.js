764c7f4c539d233f809a393df5dc64165c16279e
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-10-10 00:06:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1168,9 +1168,9 @@ const fermiQuestions = [
         answer: 36900000,
         category: "",
         explanation: "",
-        hint: "Around 24% of all visitors were Koreans and only 7.4% were Americans.",
+        hint: "Around 24% of all visitors were Korean, and 7.4% were American.",
         date: "2025-10-10",
-        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️🏰%3c/text%3e%3c/svg%3e"
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️🇯🇵%3c/text%3e%3c/svg%3e"
     },
     {
         question: "How many new medium-and heavy-duty commercial trucks were sold in the US in 2024?",
