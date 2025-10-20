5cf6f583f905e75f64d76d047a17c01e5c116776
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-10-20 23:29:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1258,9 +1258,18 @@ const fermiQuestions = [
         answer: 24519,
         category: "",
         explanation: "",
-        hint: "Harvard has around 420 thousand living alumni.",
+        hint: "Harvard has around 420,000 living alumni.",
         date: "2025-10-20",
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️🎓%3c/text%3e%3c/svg%3e"
+    },
+    {
+        question: "How many cars has Mercedes-Benz sold in the United States in 2024?",
+        answer: 324528,
+        category: "",
+        explanation: "",
+        hint: "In 2024, Tesla's US car sales totaled around 630,000.",
+        date: "2025-10-21",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️🚖%3c/text%3e%3c/svg%3e"
     },
     {
         question: "How many employees does Walmart have in the United States as of 2024?",
