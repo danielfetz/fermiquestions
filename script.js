f2cb9469235d4a8a7f6717801d5a83a3770ff254
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-10-23 00:14:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1281,7 +1281,16 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️🐪%3c/text%3e%3c/svg%3e"
     },
     {
-        question: "How many years ago was the Great Pyramid of Giza built?",
+        question: "How many police officers are there in Australia?",
+        answer: 57539,
+        category: "",
+        explanation: "The population of Australia was around 19 million in 2000.",
+        hint: "",
+        date: "2025-10-23",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️👮‍♀️%3c/text%3e%3c/svg%3e"
+    },
+    {
+        question: "How much revenue in US dollars did LVMH (make in 2024",
         answer: 4575,
         category: "",
         explanation: "",
