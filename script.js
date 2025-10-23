387668fae3c399cce63fbd304a75460dfd8d9c1d
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-10-23 23:35:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1290,6 +1290,15 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️👮‍♀️%3c/text%3e%3c/svg%3e"
     },
     {
+        question: "How many students graduated from medical school in the United Kingdom in 2024?",
+        answer: 9374,
+        category: "",
+        explanation: "There are about 3.2 practicing doctors per 1,000 people in the UK.",
+        hint: "",
+        date: "2025-10-24",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️🥼%3c/text%3e%3c/svg%3e"
+    },
+    {
         question: "How much revenue in US dollars did LVMH (make in 2024",
         answer: 4575,
         category: "",
