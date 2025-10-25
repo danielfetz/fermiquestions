d774337239d353e3624023943dbea0c92e676245
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-10-26 00:06:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1308,6 +1308,15 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️⚡️%3c/text%3e%3c/svg%3e"
     },
     {
+        question: "How much revenue in US dollars did Uber make from ridesharing services in 2024?",
+        answer: 25000000000,
+        category: "",
+        explanation: "",
+        hint: "Around 11.3 billion trips were faciliated by Uber in 2024, however this number also includes food delivery orders.",
+        date: "2025-10-26",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️🚖%3c/text%3e%3c/svg%3e"
+    },
+    {
         question: "How much revenue in US dollars did LVMH (make in 2024",
         answer: 4575,
         category: "",
