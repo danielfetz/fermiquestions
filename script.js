6a0652e253b4bd0908c08c89357c775850085041
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-11-09 00:18:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1434,6 +1434,15 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️👮‍♂️%3c/text%3e%3c/svg%3e"
     },
     {
+        question: "How much revenue in US dollars did Nike make in 2024? ",
+        answer: 51400000000,
+        category: "",
+        explanation: "",
+        hint: "In 2024, footwear revenue made up around 70% of Nike's total revenue.",
+        date: "2025-11-09",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️👟%3c/text%3e%3c/svg%3e"
+    },
+    {
         question: "How many teachers worked in schools in Germanyin the 2023/24 school year?",
         answer: 3175,
         category: "",
