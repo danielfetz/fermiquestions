3a01a36a1f223ab87d928baf241110f08186310b
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-11-10 23:03:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1452,6 +1452,15 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️💊%3c/text%3e%3c/svg%3e"
     },
     {
+        question: "How many McDonald's restaurants are there in Australia?",
+        answer: 1068,
+        category: "",
+        explanation: "",
+        hint: "There are 172 McDonald's restaurants in New Zealand.",
+        date: "2025-11-11",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️🍟%3c/text%3e%3c/svg%3e"
+    },
+    {
         question: "How many teachers worked in schools in Germanyin the 2023/24 school year?",
         answer: 3175,
         category: "",
