90884bfd024b0fb572658d6f316ee34a495ee403
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-11-18 23:55:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1524,6 +1524,15 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e🇺🇸%3c/text%3e%3c/svg%3e"
     },
     {
+        question: "How many people visited the Eiffel Tower’s observation platforms in 2024?",
+        answer: 6300000,
+        category: "",
+        explanation: "",
+        hint: "There are three public elevators that go from the ground to the observation levels.",
+        date: "2025-11-19",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e🗼%3c/text%3e%3c/svg%3e"
+    },
+    {
         question: "How many teachers worked in schools in Germanyin the 2023/24 school year?",
         answer: 3175,
         category: "",
