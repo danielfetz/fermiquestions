519ec7d6157784f7d4859c40331417d553eec1ec
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-11-20 00:08:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1533,6 +1533,15 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e🗼%3c/text%3e%3c/svg%3e"
     },
     {
+        question: "How many children aged 10 or younger live in the world today?",
+        answer: 1450000000,
+        category: "",
+        explanation: "",
+        hint: "15% of the Canadian population was aged 0-14 in 2024.",
+        date: "2025-11-20",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e👨‍👨‍👦%3c/text%3e%3c/svg%3e"
+    },
+    {
         question: "How many teachers worked in schools in Germanyin the 2023/24 school year?",
         answer: 3175,
         category: "",
