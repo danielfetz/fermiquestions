9c7624e9a6d2ab1bbbf56dac81aa2a5561811b5a
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-11-25 00:21:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1578,6 +1578,15 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e🎾%3c/text%3e%3c/svg%3e"
     },
     {
+        question: "How much revenue in US dollars did LVMH Moët Hennessy Louis Vuitton make in the first half of 2025?",
+        answer: 46800000000,
+        category: "",
+        explanation: "",
+        hint: "LVMH is the second largest company by market capitalization in the EU.",
+        date: "2025-11-24",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️👜%3c/text%3e%3c/svg%3e"
+    },
+    {
         question: "How many teachers worked in schools in Germanyin the 2023/24 school year?",
         answer: 3175,
         category: "",
