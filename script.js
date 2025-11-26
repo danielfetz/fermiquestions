08fd38f50c9695e1185fc0ba8001ea438d0cfe15
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-11-27 00:05:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1596,6 +1596,15 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️👩‍💼%3c/text%3e%3c/svg%3e"
     },
     {
+        question: "How many cans of their energy drink did Red Bull sell in 2024?",
+        answer: 12670000000,
+        category: "",
+        explanation: "",
+        hint: "Red Bull had a net income of €650 million in 2018.",
+        date: "2025-11-27",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️⚡️%3c/text%3e%3c/svg%3e"
+    },
+    {
         question: "How many teachers worked in schools in Germanyin the 2023/24 school year?",
         answer: 3175,
         category: "",
