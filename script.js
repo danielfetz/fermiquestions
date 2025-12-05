95a0e864ccb61103d5959ba1e14d0e1c1e9cb95e
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-12-06 00:36:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1677,6 +1677,15 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️👮‍♂️%3c/text%3e%3c/svg%3e"
     },
     {
+        question: "How many paying subscribers does Netflix have across Europe, Middle East & Africa?",
+        answer: 101140000,
+        category: "",
+        explanation: "",
+        hint: "Netflix had a market capitalization of around $425 billion on Dec 5, 2025.",
+        date: "2025-12-06",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️📺%3c/text%3e%3c/svg%3e"
+    },
+    {
         question: "How many teachers worked in schools in Germanyin the 2023/24 school year?",
         answer: 3175,
         category: "",
