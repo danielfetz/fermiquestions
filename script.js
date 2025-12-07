1dd1b855281468b3a41bc501bbf8dea2679fcb97
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-12-08 00:15:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1695,6 +1695,15 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️🚬%3c/text%3e%3c/svg%3e"
     },
     {
+        question: "How many students graduated from Law School in the United States in 2024?",
+        answer: 38937,
+        category: "",
+        explanation: "",
+        hint: "In 2000, about 4.06 million children were born in the United States.",
+        date: "2025-12-08",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️⚖️%3c/text%3e%3c/svg%3e"
+    },
+    {
         question: "How many teachers worked in schools in Germanyin the 2023/24 school year?",
         answer: 3175,
         category: "",
