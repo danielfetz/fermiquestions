3d38055970c297c8bb57a2055a9e27010d41910d
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-12-12 01:32:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1731,7 +1731,7 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️🐈%3c/text%3e%3c/svg%3e"
     },
     {
-        question: "How many functioning helicopters are there worldwide?",
+        question: "How many operational helicopters are there worldwide?",
         answer: 69728,
         category: "",
         explanation: "",
