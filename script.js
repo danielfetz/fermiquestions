01a730f79b4eade90449d84ab5896580711143a5
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-12-14 00:17:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1749,6 +1749,15 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️⛽️%3c/text%3e%3c/svg%3e"
     },
     {
+        question: "How many ATMs are there in the United States?",
+        answer: 530000,
+        category: "",
+        explanation: "",
+        hint: "There are roughly 44,000 ATMs (cash machines) in the United Kingdom.",
+        date: "2025-12-14",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️💳%3c/text%3e%3c/svg%3e"
+    },
+    {
         question: "How many teachers worked in schools in Germanyin the 2023/24 school year?",
         answer: 3175,
         category: "",
