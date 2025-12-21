88368a0ecd1bd2d95227106817a4366012add7a4
danielfetz <118893474+danielfetz@users.noreply.github.com>
2025-12-21 01:42:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1812,7 +1812,7 @@ const fermiQuestions = [
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f8fafc'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%23374151'%3e️🇺🇸%3c/text%3e%3c/svg%3e"
     },
     {
-        question: "How many US dollars were spent by NASA in 2024?”",
+        question: "How many US dollars were spent by NASA in 2024?",
         answer: 25000000000,
         category: "",
         explanation: "",
