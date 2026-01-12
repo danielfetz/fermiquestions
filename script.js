87df123f725b48d9e35f5e9707e6cdad2800a13a
danielfetz <118893474+danielfetz@users.noreply.github.com>
2026-01-12 23:20:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2018,6 +2018,15 @@ const fermiQuestions = [
         date: "2026-01-12",
         image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f0fdf4'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%2316a34a'%3e🛳️%3c/text%3e%3c/svg%3e"
     },
+   {
+        question: "How many hours per day were spent in total watching YouTube on TV screens in 2024?",
+        answer: 1000000000,
+        category: "",
+        explanation: "",
+        hint: "In the UK, people spent 39 minutes on YouTube per day in 2024, with 16 minutes of this via the household's TV set.",
+        date: "2026-01-13",
+        image: "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3e%3crect width='100' height='100' fill='%23f0fdf4'/%3e%3ctext x='50' y='62' font-size='40' text-anchor='middle' fill='%2316a34a'%3e📺%3c/text%3e%3c/svg%3e"
+    },
     {
         question: "How many movies with more than 1000 user ratings are listed on the movie review platform IMDb? worldwide celebrate their 50th birthday today (December 24, 2025)?",
         answer: 275000,
